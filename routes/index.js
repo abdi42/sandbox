da769a550972bc6607e1b11b93dac23a8ccf6c42
abdi42 <abdullahimahamed42@gmail.com>
2016-07-27 14:28:56 -0500

    fixed dir

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var sandbox = require("../api/sandbox.js")
 
-router.post('/',sandbox.create,sandbox.runCode);
+router.post('/',sandbox.create,sandbox.runCode,sandbox.checkCode);
 
 module.exports = router;
