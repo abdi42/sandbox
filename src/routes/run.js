df299f6dd8d707df41abafceea24d6af6c84bd8f
abdi42 <abdullahimahamed42@gmail.com>
2016-09-22 18:10:42 -0500

    custom run route

--- src/routes/run.js
+++ src/routes/run.js
@@ -1,7 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var sandbox = require("../api/sandbox.js")
+var dockerhttp = require("../lib/dockerhttp.js");
+var langs = require("../lib/langs.js");
+var cuid = require("cuid");
+var fs = require("fs");
+var exec = require("child_process").exec;
+var dockerContainer = require("../lib/container.js");
 
-router.post('/',sandbox.create,sandbox.runCode,sandbox.checkCode,sandbox.remove);
+
+router.post('/',function(req,res,callback){
+  req.body.dirname = cuid();
+
+  var config = req.body
+  config.image = "singlerun"
+  config.volume = "/codetree/tempDir"
+  config.binds = ["/home/abdullahimahamed0987/sandbox/temp/" + config.dirname + ":/codetree/tempDir:rw"]
+  config.commands = ['/bin/bash']
+
+  dockerContainer.createTemps(req.body, function(err) {
+      if (err) return callback(err)
+
+      dockerContainer.createContainer(config,function(err, containerId) {
+          if (err) return callback(err);
+
+          req.body.containerId = containerId;
+
+          return callback();
+      })
+  })
+},function(req,res,callback){
+  dockerContainer.update(req.body,function(err){
+      if(err) return callback(err)
+
+      dockerContainer.containerExec(req.body.containerId,['node','app.js'],function(err){
+          if(err) return callback(err)
+
+          return callback();
+      })
+  })
+},function(req,res,callback){
+  res.json({mssg:"done"})
+});
 
 module.exports = router;
