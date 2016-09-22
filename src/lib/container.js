3988a7d356f47e105ed93acd5ed664d8e46b7025
abdi42 <abdullahimahamed42@gmail.com>
2016-09-22 02:52:27 -0500

    testing

--- src/lib/container.js
+++ src/lib/container.js
@@ -3,6 +3,7 @@ var filesystem = require("./filesystem.js");
 var jsonfile = require("jsonfile");
 var langs = require("./langs.js");
 var fs = require("fs")
+var cuid = require("cuid");
 
 exports.createContainer = function(config,callback){
     var containerOpts = {
