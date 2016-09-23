4008335a154c4cf538b7edc1222aadb520b13e88
abdi42 <abdullahimahamed42@gmail.com>
2016-09-23 06:43:45 -0500

    changed route

--- src/routes/check.js
+++ src/routes/check.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var sandbox = require("../api/sandbox.js")
+var dockerhttp = require("../lib/dockerhttp.js");
+var langs = require("../lib/langs.js");
 var cuid = require("cuid");
+var fs = require("fs");
+var exec = require("child_process").exec;
+var dockerContainer = require("../lib/container.js");
 
 router.post('/',function(req,res,callback){
   req.body.dirname = cuid();
