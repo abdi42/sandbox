6d882ee444a01aba4dd5bf559bc270b7bc933642
abdi42 <abdullahimahamed42@gmail.com>
2016-09-22 19:03:48 -0500

    default

--- src/routes/init.js
+++ src/routes/init.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var sandbox = require("../../api/sandbox.js")
+var sandbox = require("../api/sandbox.js")
 
 router.post('/',sandbox.create,sandbox.runCode,sandbox.checkCode);
 
