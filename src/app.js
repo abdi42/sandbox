db950c601ff548d830ad612de6bda86a4100ab09
abdi42 <abdullahimahamed42@gmail.com>
2016-09-22 19:56:47 -0500

    subroute

--- src/app.js
+++ src/app.js
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/check', check);
 app.use('/run', run);
-app.use('/sandbox',sandboxRun)
-app.use('/init',sandboxInit)
+app.use('/sandbox/run',sandboxRun)
+app.use('/sandbox/init',sandboxInit)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
