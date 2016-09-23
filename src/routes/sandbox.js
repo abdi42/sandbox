ab549072d2321c7435c40f840db6dcec9f527599
abdi42 <abdullahimahamed42@gmail.com>
2016-09-22 19:45:58 -0500

    fixed syntax error

--- src/routes/sandbox.js
+++ src/routes/sandbox.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/',,sandbox.create,sandbox.runCode,sandbox.checkCode);
+router.post('/',sandbox.create,sandbox.runCode,sandbox.checkCode);
 
 module.exports = router;
