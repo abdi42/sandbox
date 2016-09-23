bfa6c546a8b2c1e0a8a661f2700e15b4087e4c31
abdi42 <abdullahimahamed42@gmail.com>
2016-09-23 06:49:13 -0500

    removing docker container

--- src/routes/sandbox/run.js
+++ src/routes/sandbox/run.js
@@ -3,7 +3,16 @@ var router = express.Router();
 var sandbox = require("../../api/sandbox.js")
 
 router.post('/',sandbox.runCode,sandbox.checkCode,function(req,res,callback){
-  res.json(req.body);
+  dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
+      if(err) res.status(500).send(stderr)
+
+      dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
+        if(err)
+          res.status(500).send(stderr)
+
+        res.json(req.body);
+      })
+  })
 });
 
 module.exports = router;
