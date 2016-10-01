869394f7d9e92267daa8671ef9086a29c0073529
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 13:11:20 -0500

    testing

--- src/routes/check.js
+++ src/routes/check.js
@@ -9,22 +9,20 @@ var exec = require("child_process").exec;
 var dockerContainer = require("../lib/container.js");
 var eval = require("../lib/eval.js")
 
-router.post('/',sandbox.create,sandbox.create,sandbox.runCode,sandbox.checkCode,removeContainer);
+router.post('/',sandbox.create,sandbox.create,sandbox.runCode,removeContainer);
 
 function removeContainer(req,res,callback){
   console.log("REMOVING")
-  dockerhttp.post("/containers/"+req.body.containerId+"/kill",{},function(err){
-    dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
-      console.log("RAN")
-        if(err) res.status(500).send(err)
+  dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
+    console.log("RAN")
+      if(err) res.status(500).send(err)
 
-        dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
-          if(err)
-            res.status(500).send(err)
+      dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
+        if(err)
+          res.status(500).send(err)
 
-          res.json(req.body.result);
-        })
-    })
+        res.json({});
+      })
   })
 }
 
