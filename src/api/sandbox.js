0ba2a5802b70dba5d45a24d2c3172d99c7f3776c
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 13:17:30 -0500

    f

--- src/api/sandbox.js
+++ src/api/sandbox.js
@@ -44,47 +44,43 @@ var Sandbox = {
     checkCode:function(req,res,callback){
       done = false;
 
-      fs.readFile("temp/"+req.body.dirname+"/compileout.txt","utf8", function(err,data) {
-          if (err) {
-            return;
-          }
-          else{
-
-            exec("rm temp/"+req.body.dirname+"/compileout.txt",function(err,stdout,stderr){
-              if(err) return callback(err)
-
-              return callback();
-            })
-
-          }
-      });
-
-
-      fs.access("temp/"+req.body.dirname+"/completed.txt", fs.F_OK, function(err) {
-          if (err) {
+        fs.readFile("temp/"+req.body.dirname+"/compileout.txt","utf8", function(err,data) {
+            if (err) {
               return;
-          }
-          else{
-            evalute(req.body.dirname,{
-              input:req.body.input,
-              expectedOutput:req.body.output
-            },function(err,result){
+            }
+            else{
 
-              if(err) return callback(err)
+              removeContainer(req,function(){
 
+                res.status(400).json({
+                  status:400,
+                  error:data
+                })
 
-              exec("rm temp/"+req.body.dirname+"/completed.txt",function(err,stdout,stderr){
-                if(err) return callback(err)
-
-                req.body.result = result;
-
-                return callback();
               })
+            }
+        });
 
-            })
 
-          }
-      });
+        fs.access("temp/"+req.body.dirname+"/completed.txt", fs.F_OK, function(err) {
+            if (err) {
+                return;
+            }
+            else{
+              removeContainer(req,function(){
+
+                var data = fs.readFileSync("temp/" + req.body.dirname + "/src/output/0.txt","utf8");
+                var outputArr = data.split("\n");
+                req.body.output = outputArr;
+
+                res.json({
+                  status:200,
+                  output:req.body.output
+                })
+
+              })
+            }
+        });
 
     },
     remove:function(req,res,callback){
@@ -108,6 +104,21 @@ function evalute(dirname,data,callback){
     return callback(null,result);
   })
 }
+
+
+function removeContainer(req,callback){
+  dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
+      if(err) return callback(stderr)
+
+      dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
+        if(err)
+          return callback(stderr)
+
+        return callback()
+      })
+  })
+}
+
 
 
 module.exports = Sandbox;
--- src/routes/run.js
+++ src/routes/run.js
@@ -9,77 +9,7 @@ var exec = require("child_process").exec;
 var dockerContainer = require("../lib/container.js");
 
 
-router.post('/',function(req,res,callback){
-  req.body.dirname = cuid();
-
-  var config = req.body
-  config.image = "coderunner"
-  config.volume = "/codetree/tempDir"
-  config.binds = ["/home/abdullahimahamed0987/sandbox/temp/" + config.dirname + ":/codetree/tempDir:rw"]
-  config.commands = ['/bin/bash']
-
-  dockerContainer.createTemps(req.body, function(err) {
-      if (err) return callback(err)
-
-      dockerContainer.createContainer(config,function(err, containerId) {
-          if (err) return callback(err);
-
-          req.body.containerId = containerId;
-
-          return callback();
-      })
-  })
-},function(req,res,callback){
-  dockerContainer.update(req.body,function(err){
-      if(err) return callback(err)
-
-      dockerContainer.containerExec(req.body.containerId,['node','app.js'],function(err){
-          if(err) return callback(err)
-
-          return callback();
-      })
-  })
-},function(req,res,callback){
-
-  fs.readFile("temp/"+req.body.dirname+"/compileout.txt","utf8", function(err,data) {
-      if (err) {
-        return;
-      }
-      else{
-
-        removeContainer(req,function(){
-
-          res.status(400).json({
-            status:400,
-            error:data
-          })
-
-        })
-      }
-  });
-
-
-  fs.access("temp/"+req.body.dirname+"/completed.txt", fs.F_OK, function(err) {
-      if (err) {
-          return;
-      }
-      else{
-        removeContainer(req,function(){
-
-          var data = fs.readFileSync("temp/" + req.body.dirname + "/src/output/0.txt","utf8");
-          var outputArr = data.split("\n");
-          req.body.output = outputArr;
-
-          res.json({
-            status:200,
-            output:req.body.output
-          })
-
-        })
-      }
-  });
-
-});
+router.post('/',sandbox.create,sandbox.create,sandbox.runCode);
 
 function removeContainer(req,callback){
   dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
