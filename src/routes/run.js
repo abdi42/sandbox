0192622dc9965398b53fceda378aa2aab57085d6
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 11:57:21 -0500

    returning error

--- src/routes/run.js
+++ src/routes/run.js
@@ -40,11 +40,36 @@ router.post('/',function(req,res,callback){
       })
   })
 },function(req,res,callback){
-  var data = fs.readFileSync("temp/" + req.body.dirname + "/src/output/0.txt","utf8");
-  var outputArr = data.split("\n");
-  req.body.output = outputArr;
 
-  return callback(null);
+  fs.readFile("temp/"+req.body.dirname+"/compileout.txt","utf8", function(err,data) {
+      if (err) {
+        return;
+      }
+      else{
+
+        exec("rm temp/"+req.body.dirname+"/compileout.txt",function(err,stdout,stderr){
+          if(err) return callback(err)
+
+          return callback();
+        })
+
+      }
+  });
+
+
+  fs.access("temp/"+req.body.dirname+"/completed.txt", fs.F_OK, function(err) {
+      if (err) {
+          return;
+      }
+      else{
+        var data = fs.readFileSync("temp/" + req.body.dirname + "/src/output/0.txt","utf8");
+        var outputArr = data.split("\n");
+        req.body.output = outputArr;
+
+        return callback(null);
+      }
+  });
+
 },function(req,res,callback){
 
   dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
