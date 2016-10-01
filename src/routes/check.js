0eb3cdc1053e1aedb115124b96a306a9a88dd7cb
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 12:17:45 -0500

    testing

--- src/routes/check.js
+++ src/routes/check.js
@@ -9,82 +9,7 @@ var exec = require("child_process").exec;
 var dockerContainer = require("../lib/container.js");
 var eval = require("../lib/eval.js")
 
-router.post('/',function(req,res,callback){
-  req.body.dirname = cuid();
-
-  var config = req.body
-  config.image = "coderunner"
-  config.volume = "/codetree/tempDir"
-  config.binds = ["/home/abdullahimahamed0987/sandbox/temp/" + config.dirname + ":/codetree/tempDir:rw"]
-  config.commands = ['/bin/bash']
-
-  dockerContainer.createTemps(req.body, function(err) {
-      if (err) return callback(err)
-
-      dockerContainer.createContainer(config,function(err, containerId) {
-          if (err) return callback(err);
-
-          req.body.containerId = containerId;
-
-          return callback();
-      })
-  })
-},function(req,res,callback){
-  dockerContainer.update(req.body,function(err){
-      if(err) return callback(err)
-
-      dockerContainer.containerExec(req.body.containerId,['node','app.js'],function(err){
-          if(err) return callback(err)
-
-          return callback();
-      })
-  })
-},function(req,res,callback){
-  done = false;
-
-  fs.readFile("temp/"+req.body.dirname+"/compileout.txt","utf8", function(err,data) {
-      if (err) {
-        return;
-      }
-      else{
-
-        exec("rm temp/"+req.body.dirname+"/compileout.txt",function(err,stdout,stderr){
-          if(err) return callback(err)
-          res.status(500).send(stderr)
-          return callback();
-        })
-
-      }
-  });
-
-
-  fs.access("temp/"+req.body.dirname+"/completed.txt", fs.F_OK, function(err) {
-      if (err) {
-          return;
-      }
-      else{
-        evalute(req.body.dirname,{
-          input:req.body.input,
-          expectedOutput:req.body.output
-        },function(err,result){
-
-          if(err) return callback(err)
-
-
-          exec("rm temp/"+req.body.dirname+"/completed.txt",function(err,stdout,stderr){
-            if(err) return callback(err)
-
-            req.body.result = result;
-
-            return callback();
-          })
-
-        })
-
-      }
-  });
-
-},function(req,res,callback){
+router.post('/',sandbox.create,sandbox.create,sandbox.runCode,sandbox.checkCode,function(req,res,callback){
   dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
       if(err) res.status(500).send(err)
 
@@ -96,13 +21,5 @@ router.post('/',function(req,res,callback){
       })
   })
 });
-
-function evalute(dirname,data,callback){
-  eval.checkFiles("temp/"+dirname+"/src/output",data.expectedOutput,function(err,result){
-    if(err) return callback(err);
-
-    return callback(null,result);
-  })
-}
 
 module.exports = router;
