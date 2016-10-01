5acd5c90ff842587adf1687cb87ce5fd6b6344a1
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 13:09:51 -0500

    killing container

--- src/routes/check.js
+++ src/routes/check.js
@@ -13,16 +13,18 @@ router.post('/',sandbox.create,sandbox.create,sandbox.runCode,sandbox.checkCode,
 
 function removeContainer(req,res,callback){
   console.log("REMOVING")
-  dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
-    console.log("RAN")
-      if(err) res.status(500).send(err)
+  dockerhttp.post("/containers/"+req.body.containerId+"/kill",{},function(err){
+    dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
+      console.log("RAN")
+        if(err) res.status(500).send(err)
 
-      dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
-        if(err)
-          res.status(500).send(err)
+        dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
+          if(err)
+            res.status(500).send(err)
 
-        res.json(req.body.result);
-      })
+          res.json(req.body.result);
+        })
+    })
   })
 }
 
