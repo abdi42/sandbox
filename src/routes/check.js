6bd9e68a2d75a62e4ea5ccf75a99d53b484ec722
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 12:56:23 -0500

    added logs

--- src/routes/check.js
+++ src/routes/check.js
@@ -12,7 +12,9 @@ var eval = require("../lib/eval.js")
 router.post('/',sandbox.create,sandbox.create,sandbox.runCode,sandbox.checkCode,removeContainer);
 
 function removeContainer(req,res,callback){
+  console.log("REMOVING")
   dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
+    console.log("RAN")
       if(err) res.status(500).send(err)
 
       dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
