a29f483cb69db5921da37ca65bf12039e9059aba
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 16:04:07 -0500

    reverted back

--- src/middleware/checkSingleRun.js
+++ src/middleware/checkSingleRun.js
@@ -8,7 +8,10 @@ module.exports = function(req,res,callback){
       if(req.body.input.length > 0){
         if(Array.isArray(req.body.input[0])){
           res.status(400)
-          return callback("Input can't be" + typeof req.body.input[0])
+          res.json({
+            status:400,
+            error:"Input can't be" + typeof req.body.input[0]
+          })
         }
         else{
           req.body.input = [req.body.input];
