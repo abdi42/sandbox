e0be13637b05aacb108e886451eb56643dc532e6
abdi42 <abdullahimahamed42@gmail.com>
2016-10-01 14:02:17 -0500

    updated code

--- src/routes/check.js
+++ src/routes/check.js
@@ -9,21 +9,6 @@ var exec = require("child_process").exec;
 var dockerContainer = require("../lib/container.js");
 var eval = require("../lib/eval.js")
 
-router.post('/',sandbox.create,sandbox.create,sandbox.runCode,removeContainer);
-
-function removeContainer(req,res,callback){
-  console.log("REMOVING")
-  dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
-    console.log("RAN")
-      if(err) res.status(500).send(err)
-
-      dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
-        if(err)
-          res.status(500).send(err)
-
-        res.json({});
-      })
-  })
-}
+router.post('/',sandbox.create,sandbox.create,sandbox.runCode,sandbox.checkCode);
 
 module.exports = router;
