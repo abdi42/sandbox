ccef4cd07e1f7b7ca135f604a952cd9d6c21b13f
mypcuser <mypcuser@Mac23.local>
2016-10-03 18:15:05 -0500

    returning container id

--- src/routes/sandbox/init.js
+++ src/routes/sandbox/init.js
@@ -2,17 +2,75 @@ var express = require('express');
 var router = express.Router();
 var sandbox = require("../../api/sandbox.js")
 
-router.post('/',sandbox.create,sandbox.runCode,sandbox.checkCode,function(req,res,callback){
+router.post('/',sandbox.create,sandbox.runCode,function(req,res,callback){
+  fs.readFile("temp/"+req.body.dirname+"/compileout.txt","utf8", function(err,data) {
+      if (err) {
+        return;
+      }
+      else{
+
+        removeContainer(req)
+
+        res.status(400).json({
+          status:400,
+          error:data
+        })
+      }
+  });
+
+  fs.readFile("temp/"+req.body.dirname+"/executionError.txt","utf8", function(err,data) {
+      if (err) {
+        return;
+      }
+      else{
+
+        removeContainer(req)
+
+        res.status(400).json({
+          status:400,
+          error:data
+        })
+      }
+  });
+
+
+  fs.access("temp/"+req.body.dirname+"/completed.txt", fs.F_OK, function(err) {
+      if (err) {
+          return;
+      }
+      else{
+        evalute(req.body.dirname,{
+          input:req.body.input,
+          expectedOutput:req.body.output
+        },function(err,result){
+
+          if(err) return callback(err);
+
+          removeContainer(req)
+
+          req.body.result = result;
+
+          res.json({
+            status:200,
+            result:req.body.result,
+            dirname:req.body.dirname,
+            containerId:req.body.containerId
+          })
+
+        })
+
+      }
+  });
+
+
+});
+
+function removeContainer(req,callback){
   dockerhttp.post("/containers/"+req.body.containerId+"/stop",{},function(err){
-      if(err) res.status(500).send(stderr)
-
       dockerhttp.delete("/containers/"+req.body.containerId,{},function(err){
-        if(err)
-          res.status(500).send(stderr)
-
-        res.json(req.body);
       })
   })
-});
+}
+
 
 module.exports = router;
