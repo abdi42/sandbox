36c192371f677f8de6fa99e0f6706ba1b673ce59
mypcuser <mypcuser@Mac23.local>
2016-10-03 17:46:28 -0500

    fixed loop error

--- src/middleware/checkMultiRun.js
+++ src/middleware/checkMultiRun.js
@@ -62,7 +62,7 @@ module.exports = function(req,res,callback){
   }
   else if(containsArray){
     for(var i=0;i<containsArray.length;i++){
-      for(var c=0;c<containsArray[i];i++){
+      for(var c=0;c<containsArray[i];c++){
         if(Array.isArray(containsArray[i][c])){
           var err = new Error("Incorrect testcases format")
           err.status = 400;
