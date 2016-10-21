42b8073b99b9d5c257d2e6751e74ec2737696c5b
abdi42 <abdullahimahamed42@gmail.com>
2016-10-20 20:20:06 -0500

    fixed error handeling

--- src/container_source/app.js
+++ src/container_source/app.js
@@ -5,6 +5,9 @@ var ArgumentParser = require('argparse').ArgumentParser;
 var parser = new ArgumentParser({
   version: '0.0.1',
 });
+var compileError = false;
+var executionError = false;
+
 parser.addArgument(
   [ '-i', '--input' ],
   {
@@ -37,14 +40,34 @@ function runCode(payload,done){
 
   program.compile(function(err){
     if(err){
-      done(err)
+      fs.writeFile("tempDir/compileout.txt",err);
+      compileError = true;
+      done(err);
     }
     else{
       program.singleRun(payload,function(err){
         if(err){
-          done(err)
+          fs.writeFile("tempDir/executionError.txt",err);
+          executionError = true;
+          done(err);
         }
-        done(null)
+
+        var file = {
+          path:"tempDir/completed.txt",
+          data:""
+        }
+
+        if(!executionError){
+          try {
+            //Creating a file synchronously
+            fs.writeFileSync(file.path, file.data, 'utf8');
+          } catch (err) {
+            done(err)
+          }
+        }
+
+        done(null);
+
       })
     }
   })
