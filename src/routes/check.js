9563ef3b25852905478416019b4434c8a47e93ca
abdi42 <abdullahimahamed42@gmail.com>
2016-12-08 11:44:55 -0600

    deleted program files

--- src/routes/check.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var sandbox = require("../api/sandbox.js")
-var checkMultiRun = require("../middleware/checkMultiRun.js")
-
-router.post('/',checkMultiRun,sandbox.create,sandbox.runCode,sandbox.checkCode);
-
-module.exports = router;
--- src/routes/run.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var sandbox = require("../api/sandbox.js")
-var checkSingleRun = require("../middleware/checkSingleRun.js")
-
-router.post('/',checkSingleRun,sandbox.create,sandbox.runCode,sandbox.getOutput);
-
-module.exports = router;
