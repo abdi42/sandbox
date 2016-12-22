5ea481fd5478b0ce3ea92b91143f7bd2f135fae3
abdi42 <abdullahimahamed42@gmail.com>
2016-12-21 23:42:31 -0600

    added container test

---
+++ test/container.js
@@ -0,0 +1,22 @@
+var checkSingleRun = require("../middleware/checkSingleRun.js")
+var kue = require('kue');
+
+queue = kue.createQueue();
+
+console.log("Running job")
+
+var job = queue.create('singleRun',{
+  source:"// extract to string\r\n#include <iostream>\r\n#include <string>\r\n\r\nint main ()\r\n{\r\n  std::string str;\r\n\r\n  std::getline (std::cin,str);\r\n\r\n  std::cout << \"Hello\\n\";\r\n  std::cout << str;\r\n\r\n  return 0;\r\n}\r\n",
+  input::[["Abdullahi"]],
+  lang:"C++"
+}).removeOnComplete(true).save();
+
+job.on('complete', function(result){
+  console.log('Job finished');
+  res.json({
+    status:200,
+    result:result
+  })
+}).on( 'error', function( err ) {
+  console.log( 'Oops... ', err );
+});
